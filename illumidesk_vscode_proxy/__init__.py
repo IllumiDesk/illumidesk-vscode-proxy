4815cc9f115e39a3eaa1c521b7b6875ab8d8aaad
Greg <werner.greg@gmail.com>
2020-08-05 02:40:21 -0400

    first commit
    Signed-off-by: Greg <werner.greg@gmail.com>

---
+++ illumidesk_vscode_proxy/__init__.py
@@ -0,0 +1,52 @@
+"""
+Source mostly obtained from https://github.com/jupyterhub/jupyter-vscode-proxy/
+
+License: BSD-3-Clause - Jupyter Team
+"""
+
+import os
+import shutil
+
+
+def setup_vscode():
+    def _get_vscode_cmd(port):
+        executable = 'code-server'
+        if not shutil.which(executable):
+            raise FileNotFoundError('Can not find code-server in PATH')
+        
+        # Start vscode in CODE_WORKINGDIR env variable if set
+        # If not, start in 'current directory', which is $REPO_DIR in mybinder
+        # but /home/jovyan (or equivalent) in JupyterHubs
+        working_dir = os.getenv('CODE_WORKINGDIR', '.')
+
+        extensions_dir = os.getenv('CODE_EXTENSIONSDIR', None)
+        extra_extensions_dir = os.getenv('CODE_EXTRA_EXTENSIONSDIR', None)
+
+        cmd = [
+            executable,
+            '--auth',
+            'none',
+            '--disable-telemetry',
+            '--port=' + str(port),
+        ]
+
+        if extensions_dir:
+            cmd += ['--extensions-dir', extensions_dir]
+
+        if extra_extensions_dir:
+            cmd += ['--extra-extensions-dir', extra_extensions_dir]
+
+        cmd.append(working_dir)
+        return cmd
+
+    return {
+        'command': _get_vscode_cmd,
+        'timeout': 20,
+        'new_browser_tab': True,
+        'launcher_entry': {
+            'title': 'VS Code',
+            'icon_path': os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), 'icons', 'vscode.svg'
+            ),
+        },
+    }
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="illumidesk-vscode-proxy",
+    version='0.1.0',
+    url="https://github.com/jupyterhub/illumidesk-vscode-proxy",
+    author="IllumiDesk Team",
+    description="hello@illumidesk.com",
+    packages=setuptools.find_packages(),
+	keywords=['Jupyter', 'IllumiDesk', 'VS Code'],
+	classifiers=['Framework :: Jupyter'],
+    install_requires=[
+        'jupyter-server-proxy'
+    ],
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'illumidesk-vscode-proxy = illumidesk_vscode_proxy:setup_illumidesk_vscode_proxy',
+        ]
+    },
+    package_data={
+        'illumidesk_vscode_proxy': ['icons/*'],
+    },
+)
